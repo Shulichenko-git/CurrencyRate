1806f410d3e4d9530bc5f33c300b5a288513c15e
Shulichenko-git <shulichenko.a@gmail.com>
2022-06-24 20:51:48 +0300

    dotenv added

--- CurrencyRate/settings.py
+++ CurrencyRate/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 # ALLOWED_HOSTS = []
 
 
